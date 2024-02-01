b4807dd455af6a3c868505282514c4cad6f57e4b
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2024-02-01 20:29:25 +0200

    Add Array & Object

--- Section3/build/js/main.js
+++ Section3/build/js/main.js
@@ -1,10 +1,41 @@
 "use strict";
 //The TypeScript compiler checks for type errors and other issues during the compilation process
-let myName = "Alaa";
-let meaningOfLife;
-let isLoading;
-let album;
-myName = "Arwa";
-meaningOfLife = 4;
-isLoading = true;
-album = 5;
+//Array:
+let strArr = ['one', 'two', 'three'];
+let mixedDate = ['A', 1, true];
+strArr[0] = 'four';
+console.log(strArr);
+let arr = [];
+//Tuples: Is specific than array
+let myTuple = ['Alaa', 26, true];
+let mixed = ['Arwa', 19, true];
+myTuple[1] = 25; // Can't change type of element
+mixed[1] = true; // Can change type of element
+//Objects
+let myObj;
+myObj = [];
+console.log(typeof myObj);
+let obj1 = {
+    name: 'Mona',
+    active: true,
+    albums: [11, 'A', 1],
+};
+let obj2 = {
+    name: 'Ali',
+    // active: false,
+    albums: [22, 'B', 2],
+};
+obj1 = obj2;
+const greetFriends = (friend) => {
+    return `Hello ${friend.name}!`;
+};
+console.log(greetFriends(obj1));
+//Enum
+var Grade;
+(function (Grade) {
+    Grade[Grade["U"] = 0] = "U";
+    Grade[Grade["D"] = 1] = "D";
+    Grade[Grade["C"] = 2] = "C";
+    Grade[Grade["B"] = 3] = "B";
+    Grade[Grade["A"] = 4] = "A";
+})(Grade || (Grade = {}));
--- Section3/src/main.ts
+++ Section3/src/main.ts
@@ -1,10 +1,57 @@
 //The TypeScript compiler checks for type errors and other issues during the compilation process
 
 
-//Array
+//Array:
 let strArr = ['one', 'two', 'three'];
 let mixedDate = ['A', 1, true];
 
 strArr[0] = 'four';
+console.log(strArr);
 
 let arr: string[] = [];
+
+//Tuples: Is specific than array
+let myTuple: [string, number, boolean] = ['Alaa', 26, true];
+let mixed = ['Arwa', 19, true];
+myTuple[1] = 25 // Can't change type of element
+mixed[1] = true // Can change type of element
+
+//Objects
+let myObj: object;
+myObj = [];
+console.log(typeof myObj);
+
+type friends = {
+    name: string,
+    active?: boolean,
+    albums: (string | number)[],
+}
+
+let obj1: friends = {
+    name: 'Mona',
+    active: true,
+    albums: [11, 'A', 1],
+}
+let obj2: friends = {
+    name: 'Ali',
+    // active: false,
+    albums: [22, 'B', 2],
+}
+
+obj1 = obj2;
+
+const greetFriends = (friend: friends) => {
+    return `Hello ${friend.name}!`
+}
+console.log(greetFriends(obj1));
+
+//Enum
+
+enum Grade{
+    U,
+    D,
+    C,
+    B,
+    A,
+}
+
