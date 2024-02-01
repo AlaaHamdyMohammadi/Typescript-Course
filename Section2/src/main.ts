a4c382e41bdbb0b7ebc88afe6a63b9af1476a9b6
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2024-02-01 04:09:01 +0200

    Basic types

---
+++ Section2/src/main.ts
@@ -0,0 +1,12 @@
+//The TypeScript compiler checks for type errors and other issues during the compilation process
+
+let myName: string = "Alaa";
+let meaningOfLife: number;
+let isLoading: boolean;
+let album: any;
+
+myName = "Arwa";
+meaningOfLife = 4;
+isLoading = true;
+album = 5;
+
