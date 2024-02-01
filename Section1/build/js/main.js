aeade59a20b2ab7211b5edc13e00064d5112ea9c
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2024-02-01 03:39:58 +0200

    Add types

--- Section1/build/js/main.js
+++ Section1/build/js/main.js
@@ -1,3 +1,8 @@
 "use strict";
+//The TypeScript compiler checks for type errors and other issues during the compilation process
 const username = "Alaa";
 console.log(username);
+const a = 6;
+const b = "6";
+const c = 6;
+console.log(a / b);
--- Section1/src/main.ts
+++ Section1/src/main.ts
@@ -1,2 +1,10 @@
+//The TypeScript compiler checks for type errors and other issues during the compilation process
+
 const username = "Alaa";
 console.log(username);
+
+const a: number = 6;
+const b: string = "6";
+const c: number = 6;
+
+console.log(a / b);
