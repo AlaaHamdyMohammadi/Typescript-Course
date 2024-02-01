0ef96981c85383597c656315a68c2034302a1a89
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2024-02-01 04:51:54 +0200

    Add Array

---
+++ Section3/build/js/main.js
@@ -0,0 +1,10 @@
+"use strict";
+//The TypeScript compiler checks for type errors and other issues during the compilation process
+let myName = "Alaa";
+let meaningOfLife;
+let isLoading;
+let album;
+myName = "Arwa";
+meaningOfLife = 4;
+isLoading = true;
+album = 5;
---
+++ Section3/src/main.ts
@@ -0,0 +1,10 @@
+//The TypeScript compiler checks for type errors and other issues during the compilation process
+
+
+//Array
+let strArr = ['one', 'two', 'three'];
+let mixedDate = ['A', 1, true];
+
+strArr[0] = 'four';
+
+let arr: string[] = [];
