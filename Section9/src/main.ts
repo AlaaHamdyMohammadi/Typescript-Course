bfef3b16507ef2617dbb0bf754933057b1083ef9
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2024-02-04 23:48:13 +0200

    Add Utility Types(2- Required and Readonly)

--- Section9/src/main.ts
+++ Section9/src/main.ts
@@ -17,4 +17,20 @@ const updateAssinment = (
 };
 
 const assign1: Assignment = { studentId: "abc123", title: "Math", grade: 0 };
-console.log(updateAssinment(assign1, {title: 'A'}))
+console.log(updateAssinment(assign1, { title: "A" }));
+/*
+  2- Required and Readonly:
+  Required:  All properties are required (verify property is optional but now is required)
+  Readonly:  All properties are readonly
+*/
+const recordAssignment = (assign: Required<Assignment>) => {
+  // Send to database
+  return assign;
+};
+
+const assignVerified: Readonly<Assignment> = {
+  ...assign1,
+  verified: true,
+};
+
+assignVerified.grade = 100; // Error => just readonly
