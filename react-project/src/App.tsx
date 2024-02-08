ff0400c32cc3c563959989a44b3b44a9ad7ab6fe
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2024-02-08 22:01:00 +0200

    Update React Project

--- react-project/src/App.tsx
+++ react-project/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Heading from "./components/Heading";
 import Section from "./components/Section";
 import Counter from "./components/Counter";
+import List from "./components/List";
 
 export default function App() {
   const [count, setCount] = useState(0);
@@ -14,6 +15,10 @@ export default function App() {
         <p>LoremChildren</p>{" "}
       </Section>
       <Counter setCount={setCount}>Count = {count}</Counter>
+      <List
+        items={["a", "b", "c"]}
+        render={(item: string) => <span className="gold">{item}</span>}
+      />
     </div>
   );
 }
